b75e077f5d8fc9450b2b78e05eb1320df327820f
Ariana Huhko <arianahuhko@gmail.com>
2024-02-26 15:12:08 -0500

    resizing script

---
+++ pretrain_data.py
@@ -0,0 +1,27 @@
+from PIL import Image
+import os
+
+def resize_image(input_folder, output_folder, size=(256, 256)):
+    for file in os.listdir(input_folder):
+        if file.lower().endswith(('.png', '.jpg', '.jpeg')): 
+            input_path = os.path.join(input_folder, file) 
+            output_path = os.path.join(output_folder, file) 
+                
+            with Image.open(input_path) as img:
+                resized_img = img.resize(size)
+                resized_img.save(output_path)
+
+
+def main():
+    input_folder = "images" 
+    
+    if not os.path.exists('resized'):
+        os.makedirs('resized') 
+        
+    output_folder = 'resized'  
+    
+    resize_image(input_folder, output_folder)
+    
+    
+if __name__ == "__main__":
+    main()
